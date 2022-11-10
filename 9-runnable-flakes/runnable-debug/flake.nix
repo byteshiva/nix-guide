101d315fbe70f4ed1ec929f6e23fbd584ba3b6a5
Tony Finn <git@tonyfinn.com>
2022-11-10 22:37:34 +0000

    Second part 9 example

--- 9-runnable-flakes/runnable-debug/flake.nix
+++ 9-runnable-flakes/runnable-debug/flake.nix
@@ -1,27 +1,28 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.debug = pkgs.stdenv.mkDerivation {
-        src = ./rust-hello;
-        name = "rust-hello-debug-1.0";
-        inherit system;
-        # Set so `nix run` can run this output
-        meta.mainProgram = "rust-hello";
-        nativeBuildInputs = [ pkgs.cargo ];
-        buildPhase = ''
-          cargo build
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp target/debug/rust-hello $out/bin/rust-hello
-          chmod +x $out
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.${system}.debug = pkgs.stdenv.mkDerivation {
+      src = ./rust-hello;
+      name = "rust-hello-debug-1.0";
+      inherit system;
+      # Set so `nix run` can run this output
+      meta.mainProgram = "rust-hello";
+      nativeBuildInputs = [pkgs.cargo];
+      buildPhase = ''
+        cargo build
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp target/debug/rust-hello $out/bin/rust-hello
+        chmod +x $out
+      '';
     };
+  };
 }
