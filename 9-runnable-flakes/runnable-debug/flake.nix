d1ee81cdbf70d3025b58e10e4bc9e78ff0c37c31
Tony Finn <git@tonyfinn.com>
2022-11-10 21:54:05 +0000

    Add runnable rust debug example

---
+++ 9-runnable-flakes/runnable-debug/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      debug = pkgs.stdenv.mkDerivation {
+        src = ./rust-hello;
+        name = "rust-hello-debug-1.0";
+        inherit system;
+        # Set so `nix run` can run this output
+        meta.mainProgram = "rust-hello";
+        nativeBuildInputs = [ pkgs.cargo ];
+        buildPhase = ''
+          cargo build
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/debug/rust-hello $out/bin/rust-hello
+          chmod +x $out
+        '';
+      };
+    };
+}
